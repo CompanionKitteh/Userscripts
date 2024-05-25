f0a9af5ce101c60ea6ea8003fb7d2f76511f5638
CompanionKitteh <31017471+CompanionKitteh@users.noreply.github.com>
2024-05-24 21:39:50 -0700

    Update Mercari Currency Updater.user.js
    Fix for non-en site

--- Mercari Currency Updater.user.js
+++ Mercari Currency Updater.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Mercari Currency Updater
 // @namespace    http://companionkitteh.com/
-// @version      0.1
+// @version      0.2
 // @description  Show JPY item price
 // @author       CompanionKitteh
+// @match        https://jp.mercari.com/search*
 // @match        https://jp.mercari.com/en/search*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mercari.com
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -11,7 +12,7 @@
 // ==/UserScript==
 
 function updateCurrency(item) {
-    let info = item[0].ariaLabel.match(/([\d,]+)(yen)/);
+    let info = item[0].ariaLabel.match(/([\d,]+)(yen|円)/);
     item[0].getElementsByClassName("number__6b270ca7")[0].innerHTML = info[1];
     item[0].getElementsByClassName("currency__6b270ca7")[0].innerHTML = "JPY"; // info[2]
 }

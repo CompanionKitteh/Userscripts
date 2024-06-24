9b3af9a935ab997b5cf23307f77c24c197a05911
CompanionKitteh <31017471+CompanionKitteh@users.noreply.github.com>
2024-06-23 22:17:59 -0700

    Update Buyee Cart Clicker.user.js
    Only use cart URL

--- Buyee Cart Clicker.user.js
+++ Buyee Cart Clicker.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Buyee Cart Clicker
 // @namespace    http://companionkitteh.com/
-// @version      0.1
+// @version      0.2
 // @description  It clicks
 // @author       CompanionKitteh
-// @match        https://buyee.jp/myshopping/cart*
+// @match        https://buyee.jp/myshopping/cart
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=buyee.jp
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==

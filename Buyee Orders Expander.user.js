99457f4813b073a5fb2367f694a3e3c36be764a8
CompanionKitteh <31017471+CompanionKitteh@users.noreply.github.com>
2025-03-10 19:54:32 -0700

    Update Buyee Orders Expander.user.js
    Add more URLs

--- Buyee Orders Expander.user.js
+++ Buyee Orders Expander.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Buyee Orders Expander
 // @namespace    http://companionkitteh.com/
-// @version      1.0
+// @version      2.0
 // @downloadURL  https://github.com/CompanionKitteh/Userscripts/raw/refs/heads/main/Buyee%20Orders%20Expander.user.js
 // @updateURL    https://github.com/CompanionKitteh/Userscripts/raw/refs/heads/main/Buyee%20Orders%20Expander.user.js
 // @description  Automatically expand orders page
 // @author       CompanionKitteh
-// @match        https://buyee.jp/myshopping/orders*
+// @match        https://buyee.jp/myshopping/orders
+// @match        https://buyee.jp/myurlpurchase/orders
+// @match        https://buyee.jp/myorders/bids/successful
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=buyee.jp
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
